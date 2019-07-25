e6207eda0626b07316ce99403d9803aa37f88ae2
Tim Vlasuk <timvlasuk@gmail.com>
2019-07-25 13:02:00 -0700

    updated the example codes model definition with ES6 class format

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,38 @@
 const Sequelize = require('sequelize');
 const { sequelize } = require('../db/sequelize');
 
-// const restaurant = (sequelize) => {
 
-const Restaurant = sequelize.define('Restaurant', {
+
+class Restaurant extends Sequelize.Model {
+
+  apiRepr() {
+    return {
+      id: this.id,
+          name: this.name,
+          cuisine: this.cuisine,
+          borough: this.borough,
+          address: {
+          number: this.addressBuildingNumber,
+          street: this.addressStreet,
+          zip: this.addressZipcode,
+      },
+    mostRecentGrade: this.mostRecentGrade ? this.mostRecentGrade.apiRepr() : null
+    }
+  }
+
+  static mostRecentGrade() {
+    let mostRecent = null;
+    (this.grades || []).forEach(grade => {
+      if (!mostRecent || grade.inspectionDate > mostRecent.inspectionDate) {
+        mostRecent = grade;
+      }
+    })
+    return mostRecent;
+  }
+
+};
+  
+Restaurant.init({
   name: {
     type: Sequelize.TEXT,
     allowNull: false
@@ -22,99 +51,22 @@ const Restaurant = sequelize.define('Restaurant', {
     type: Sequelize.TEXT,
     field: 'address_zipcode'
   }
-}, {
+}, 
+{
+  sequelize,
   tableName: 'restaurants',
-  underscored: true,
-  getterMethods: {
-    mostRecentGrade: function() {
-      let mostRecent = null;
-      (this.grades || []).forEach(grade => {
-        if (!mostRecent || grade.inspectionDate > mostRecent.inspectionDate) {
-          mostRecent = grade;
-        }
-      })
-      return mostRecent;
-    }
-  }
-}
-// , {
+  underscored: true
+  
+});
 
-  // *V* below method is pre-version 4 style : now class and instance methods are defined outside of options *V*
-  // classMethods: {
-  //   associate: function(models) {
-  //     Restaurant.hasMany(
-  //       models.Grade,
-  //       {
-  //         as: 'grades',
-  //         foreignKey: { allowNull: false },
-  //         onDelete: 'CASCADE'
-  //       }
-  //     );
-  //   }
-  // },
-
-  // instanceMethods: {
-  //   apiRepr: function() {
-  //     return {
-  //       id: this.id,
-  //       name: this.name,
-  //       cuisine: this.cuisine,
-  //       borough: this.borough,
-  //       address: {
-  //         number: this.addressBuildingNumber,
-  //         street: this.addressStreet,
-  //         zip: this.addressZipcode
-  //       },
-  //       mostRecentGrade: this.mostRecentGrade ? this.mostRecentGrade.apiRepr() : null
-  //     }
-  //   }
-//   }
-// }
-
-);
-
-Restaurant.prototype.apiRepr = () => {
-    return {
-      id: this.id,
-          name: this.name,
-          cuisine: this.cuisine,
-          borough: this.borough,
-          address: {
-          number: this.addressBuildingNumber,
-          street: this.addressStreet,
-          zip: this.addressZipcode,
-      },
-    mostRecentGrade: this.mostRecentGrade ? this.mostRecentGrade.apiRepr() : null
-    }
-  }
-
-
-  Restaurant.associate = models => {
-    Restaurant.hasMany(
-      models.Grade, 
-      {
-        as: 'grades',
-        foreignKey: { allowNull: false },
-        onDelete: 'CASCADE'
-      })
-  };
-
-  // Restaurant.apiRepr = () => {
-  //   return {
-  //     id: this.id,
-  //         name: this.name,
-  //         cuisine: this.cuisine,
-  //         borough: this.borough,
-  //         address: {
-  //         number: this.addressBuildingNumber,
-  //         street: this.addressStreet,
-  //         zip: this.addressZipcode,
-  //     },
-  //   mostRecentGrade: this.mostRecentGrade ? this.mostRecentGrade.apiRepr() : null
-  //   }
-  // }
-
-// return Restaurant
-// }
+Restaurant.associate = models => {
+  Restaurant.hasMany(
+    models.Grade, 
+    {
+      as: 'grades',
+      foreignKey: { allowNull: false },
+      onDelete: 'CASCADE'
+    })
+};
 
 module.exports =  Restaurant ;
